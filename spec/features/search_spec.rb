a45f53f8392a6bb1a22c392792404749edf29676
Jane Greene <janegreenecasa@gmail.com>
2020-09-01 10:32:05 -0600

    Made test a little more realistic for nil values

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -9,33 +9,17 @@ RSpec.feature 'user can search for order of the phoenix members by house' do
 
     expect(page).to have_content("There are 21 members of the Order of the Phoenix")
     expect(page).to have_css('.member', count: 21)
-    save_and_open_page
     within(first('.member')) do
-      within('.name') do
-        # expect(page).to have_content("")
-      end
-      within('.role') do
-        # expect(page).to have_content("")
-      end
-      within('.house') do
-        # expect(page).to have_content("")
-      end
-      within('.patronus') do
-        # expect(page).to have_content("")
-      end
+        expect(page).to have_css('.role')
+        expect(page).to have_css('.patronus')
+
+        expect(page).to have_css('.name')
+        name = find('.name').text
+        expect(name).not_to be_empty
+
+        expect(page).to have_css('.house')
+        house = find('.house').text
+        expect(house).not_to be_empty
     end
   end
 end
-# As a user,
-# When I visit "/"
-# And I Select "Gryffindor" from the select field
-# (Note: Use the existing select field)
-# And I click "Search For Members"
-# Then I should be on page "/search"
-# Then I should see the total number of the order of the phoenix members that belong to Gryffindor. (i.e. 21)
-# And I should see a list with the detailed information for the 21 members of the Order of the Phoenix for house Gryffindor.
-# And for each of the members I should see:
-# - The name of the member
-# - The members role (if it exists)
-# - The house the member belongs to
-# - The Patronus of the member (if it exists)
